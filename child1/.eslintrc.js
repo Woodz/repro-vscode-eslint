bad6da4658d4835088d2569f816ab7789753dbe3
richard.wise <966157+Woodz@users.noreply.github.com>
2023-01-13 13:30:47 +0800

    Initial project setup

---
+++ child1/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  // TechDebt: Find nicer way to ignore compiled files
+  ignorePatterns: ['**/dist/**/*.*'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'plugin:prettier/recommended'
+  ]
+};
